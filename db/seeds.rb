92ba74c189033479986284e4c2f481dc5d6ca667
penyakamaayo <elcabron.penyah@gmail.com>
2022-04-18 17:18:14 +0800

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,9 @@ User.populate 50 do |u|
   u.encrypted_password = User.new(:password => 'testing 123').encrypted_password
   u.role = 1
 end
+
+User.create([
+  { firstname: 'Doc', lastname: 'Vic', mobile_number: '9676514239', password: '123456', password_confirmation: '123456', role: 'doctor' },
+  { firstname: 'Sec', lastname: 'Vangie', mobile_number: '9289185284', password: '123456', password_confirmation: '123456', role: 'secretary' },
+  { firstname: 'Jose', lastname: 'Peña', mobile_number: '9973806704', password: '123456', password_confirmation: '123456' }
+])
