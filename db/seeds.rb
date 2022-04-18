2c164cf894c2bcf9308edce775eab13b386465a7
penyakamaayo <elcabron.penyah@gmail.com>
2022-04-18 17:29:35 +0800

    remove faker for prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require_relative '../lib/populator_fix.rb'
-require 'faker'
+# require_relative '../lib/populator_fix.rb'
+# require 'faker'
 
-User.populate 50 do |u|
-  u.firstname = Faker::Name.first_name
-  u.lastname = Faker::Name.unique.last_name
-  u.mobile_number = Faker::PhoneNumber.phone_number
-  u.email = Faker::Internet.email
-  u.gender = Faker::Gender.binary_type
-  u.encrypted_password = User.new(:password => 'testing 123').encrypted_password
-  u.role = 1
-end
+# User.populate 50 do |u|
+#   u.firstname = Faker::Name.first_name
+#   u.lastname = Faker::Name.unique.last_name
+#   u.mobile_number = Faker::PhoneNumber.phone_number
+#   u.email = Faker::Internet.email
+#   u.gender = Faker::Gender.binary_type
+#   u.encrypted_password = User.new(:password => 'testing 123').encrypted_password
+#   u.role = 1
+# end
 
 User.create([
   { firstname: 'Doc', lastname: 'Vic', mobile_number: '9676514239', password: '123456', password_confirmation: '123456', role: 'doctor' },
